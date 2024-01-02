4fe47f797aca40e9ca39088ffcd996a0a2feccb6
Gawerek <michal.gawron98@gmail.com>
2024-01-02 16:22:25 +0100

    test_delete_image

--- PagesSP/ProfilePortfolioScreenSP.py
+++ PagesSP/ProfilePortfolioScreenSP.py
@@ -14,6 +14,8 @@ class ProfilePortfolioScreenSP(BasePageSP):
     photo_locator = (AppiumBy.ACCESSIBILITY_ID, "hair-1.jpg, 131 KB, 15:29")
     crop_button = (AppiumBy.ID, "bbox.pl.sp.app.development:id/crop_image_menu_crop")
     description_input = (AppiumBy.ACCESSIBILITY_ID, "profile-gallery-description-input-1")
+    delete_image_button =(AppiumBy.ACCESSIBILITY_ID,"profile-gallery-delete-image-button-1")
+
 
     def add_image(self):
         self.click(self.add_image_button)
@@ -21,3 +23,6 @@ class ProfilePortfolioScreenSP(BasePageSP):
         self.click(self.photo_locator)
         self.click(self.crop_button)
         self.type(self.description_input, 'dupa123')
+
+    def delete_image(self):
+        self.click(self.delete_image_button)
